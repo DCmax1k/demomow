2d4c760f76b739491f95e025ed330ae0d9742e22
Dylan Caldwell <dylan.caldwell35@gmail.com>
2023-02-12 16:34:02 -0500

    Added form data collection. Submit success feedback for user. Data does not upload yet

--- client/src/components/HomePage/FormPage.js
+++ client/src/components/HomePage/FormPage.js
@@ -15,16 +15,38 @@ class FormPage extends Component {
         this.state = {
             type: props.type, // '', 'mow', 'tree'
             text: true,
+            feedbackActive: '',
         };
         this.toggleText = this.toggleText.bind(this);
+        this.submit = this.submit.bind(this);
 
         this.name = React.createRef();
+        this.phone = React.createRef();
+        this.address = React.createRef();
+        this.desc = React.createRef();
     }
 
     toggleText() {
         this.setState({
             text: !this.state.text
         })
+    }
+
+    submit() {
+        const data = {
+            type: this.state.type,
+            name: this.name.current.getValue(),
+            phone: this.phone.current.getValue(),
+            address: this.address.current.getValue(),
+            desc: this.desc.current.getValue(),
+            date: new Date(),
+        };
+        console.log(data);
+
+        // If successfully sent request
+        this.setState({
+            feedbackActive: 'active',
+        });
     }
 
     render() {
@@ -54,21 +76,27 @@ class FormPage extends Component {
 
                     {/* Inputs */}
                     <Input key="name" ref={this.name} placeholder="Name" />
-                    <Input key="phone" ref={this.name} placeholder="Phone">
+                    <Input key="phone" ref={this.phone} placeholder="Phone">
                         <div className='textCheck' onClick={this.toggleText} >
                             <img alt='text bool' className='textCheck' src={this.state.text===true?'/textCheck.svg':'/textX.svg'} />
                         </div>
                     </Input>
-                    <Input key="address" ref={this.name} placeholder="Address of job location"/>
-                    <TextArea key="desc" ref={this.name} placeholder="Brief job description"/>
+                    <Input key="address" ref={this.address} placeholder="Address of job location"/>
+                    <TextArea key="desc" ref={this.desc} placeholder="Brief job description"/>
 
-                    <ColoredBorderButton color={this.state.type==='mow'?'red':'blue'} width='100%' height='60px'>
+                    <ColoredBorderButton onClick={this.submit} color={this.state.type==='mow'?'red':'blue'} width='100%' height='60px'>
                         Request!
                     </ColoredBorderButton>
 
                     <div className='disclaimer'>
                         <FontAwesomeIcon icon={faCircleInfo} />
                         <p>We will attempt to text and/or call you as soon as we get the chance!</p>
+                    </div>
+
+                    {/* Submit feedback */}
+                    <div className={`submitFeedback ${this.state.feedbackActive}`}>    
+                        <h2>Your request has been successfully submitted!</h2>
+                        <p>---- Expect a response within 1-3 business days ----</p>
                     </div>
 
 
